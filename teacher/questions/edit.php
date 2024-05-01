6f23e184747617543f7b5079c03122625b7172c0
Asad6770 <asad.879427@gmail.com>
2024-05-02 00:48:44 +0500

    commit

--- teacher/questions/edit.php
+++ teacher/questions/edit.php
@@ -8,12 +8,13 @@ $row = $data[0];
 $quiz = select('quiz', '*');
 $question_id = 'question_id=' . $row['id'];
 $options = select('options', '*', $question_id);
+// print_r($options);
 
 ?>
 
 <form action="process.php" method="post" enctype="multipart/form-data" class="submitData" autocomplete="off">
-    <input type="hidden" class="form-control" name="type" value="edit">
-    <input type="hidden" class="form-control" name="id" value="<?= $_GET['id'] ?>">
+    <input type="hidden" name="type" value="edit">
+    <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
 
     <div class="form-group">
         <label for="quiz_id">Select Quiz</label>
@@ -34,14 +35,15 @@ $options = select('options', '*', $question_id);
     </div>
 
     <div class="form-group">
-        <label class="font-weight-bold" for="description">Question Description</label>
-        <input type="text" class="form-control" name="description" value="<?= $row['question']; ?>">
+        <label class="font-weight-bold" for="question">Question Description</label>
+        <input type="text" class="form-control" name="question" value="<?= $row['question']; ?>">
         <small class="error description_error text-danger font-weight-bold" style="font-size: 15px;"></small>
     </div>
     <?php foreach ($options as $key => $value) { ?>
+        <input type="hidden" name="option_id[]" value="<?= $value['id']; ?>">
         <div class="form-group">
-            <label class="font-weight-bold" for="correct_option">Option <?= $key + 1 ?></label>
-            <input type="text" class="form-control" name="correct_option[]" value="<?= $value['option']; ?>">
+            <label class="font-weight-bold" for="option">Option <?= $key + 1 ?></label>
+            <input type="text" class="form-control" name="option[]" value="<?= $value['option']; ?>">
             <small class="error total_score_error text-danger font-weight-bold" style="font-size: 15px;"></small>
         </div>
     <?php } ?>
@@ -50,10 +52,10 @@ $options = select('options', '*', $question_id);
         <label class="font-weight-bold mr-3" for="is_correct">Select Correct Option</label>
         <select class="form-control" name="is_correct" id="is_correct">
             <option value="">Select Correct Option</option>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-            <option value="4">Option 4</option>
+            <option value="1" <?=('1' == $options[0]['is_correct']) ? 'selected' : ''?>>Option 1</option>
+            <option value="2" <?=('1' == $options[1]['is_correct']) ? 'selected' : ''?>>Option 2</option>
+            <option value="3" <?=('1' == $options[2]['is_correct']) ? 'selected' : ''?>>Option 3</option>
+            <option value="4" <?=('1' == $options[3]['is_correct']) ? 'selected' : ''?>>Option 4</option>
         </select>
         <small class=" error is_correct_error text-danger font-weight-bold" style="font-size: 15px;"></small>
     </div>
--- teacher/questions/process.php
+++ teacher/questions/process.php
@@ -27,7 +27,7 @@ if (@$_POST['type'] == 'create') {
         $sql = "INSERT INTO questions (quiz_id, question) VALUES ('$quiz_id', '$question')";
         $conn->query($sql);
         $question_id = $conn->insert_id;
-  
+
         for ($i = 0; $i < count($_POST['option']); $i++) {
             $is_correct = ($i + 1 == $correct) ? 1 : 0;
             $data = [
@@ -54,19 +54,32 @@ if (@$_POST['type'] == 'edit') {
         $errors['quiz_id'] = "Quiz Field is Required!";
     }
 
-    if (empty($_POST['description'])) {
-        $errors['description'] = "Question Field is Required!";
+    if (empty($_POST['question'])) {
+        $errors['question'] = "Question Field is Required!";
     }
     if (empty($_POST['correct_option'])) {
-        $errors['correct_option'] = "Correct Option Title Field is Required!";
+        $errors['correct_option'] = "Correct Option Field is Required!";
+    }
+    if (empty($_POST['is_correct'])) {
+        $errors['is_correct'] = "Correct Option Field is Required!";
     } else {
-        $data = [
-            'quiz_id' => $_POST['quiz_id'],
-            'description' => $_POST['description'],
-            'correct_option' => $_POST['correct_option'],
-        ];
-        $where = 'id= ' . $_POST['id'];
-        $update = update('questions', $data, $where);
+        $question = $_POST['question'];
+        $quiz_id = $_POST['quiz_id'];
+        $correct = $_POST['is_correct'];
+        $id = $_POST['id'];
+        $sql = "UPDATE questions SET quiz_id = '$quiz_id', question = '$question' WHERE id = $id";
+        // print_r($sql);
+        // exit();
+        $conn->query($sql);
+        for ($i = 0; $i < count($_POST['option']); $i++) {
+            $is_correct = ($i +1 == $correct) ? 1 : 0;
+            $where = 'id= ' . $_POST['option_id'][$i];
+            $data = [
+                'option' => $_POST['option'][$i],
+                'is_correct' => $is_correct,
+            ];
+            $update = update('options', $data, $where);
+        }
         echo json_encode($update);
         exit();
     }
